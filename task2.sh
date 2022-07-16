bf8beb6da16bbfc76e89913ad21d1fe31b2f0ed0
PeanutYC <72436355+PeanutYC@users.noreply.github.com>
2022-07-16 13:25:34 +0800

    Delete task2.sh

--- task2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rosrun sound_play soundplay_node.py &
-rosrun task1_2 ask_name_synthesis.py &
-rosrun task1_2 ask_name_sr.py 
-roslaunch rchomeedu_vision multi_astra.launch &
-roslaunch robot_vision_openvino interactive_face_ros.launch & 
-rosrun task1_2 say_gender_synthesis.py
-
-rosnode kill -a
